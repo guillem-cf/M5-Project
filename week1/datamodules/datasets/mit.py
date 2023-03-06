74ef7ee2de020de2e4afe002f4d7df2604756c34
johnnynunez <johnnynuca14@gmail.com>
2023-03-07 00:56:21 +0100

    example model

--- week1/datamodules/datasets/mit.py
+++ week1/datamodules/datasets/mit.py
@@ -1,21 +1,7 @@
-from torch.utils.data import Dataset
+from week1.datamodules.datasets.base_dataset import BaseDataset
 
 
-class MITDataset(Dataset):
+class MITDataset(BaseDataset):
     def __init__(self, data, targets, transform=None, target_transform=None):
-        self.data = data
-        self.targets = targets
-        self.transform = transform
-        self.target_transform = target_transform
+        super().__init__(data, targets, transform, target_transform)
 
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, index):
-        image = self.data[index]
-        label = self.targets[index]
-        if self.transform:
-            image = self.transform(self.data[index])
-        if self.target_transform:
-            label = self.transform(self.targets[index])
-        return image, label
