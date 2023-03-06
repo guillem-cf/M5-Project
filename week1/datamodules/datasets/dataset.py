55225fa234a7c708b783b0be7a35b727f90ea9a7
johnnynunez <johnnynuca14@gmail.com>
2023-03-06 22:48:51 +0100

    nvitop

---
+++ week1/datamodules/datasets/dataset.py
@@ -0,0 +1,13 @@
+import torch
+from torch.utils.data import Dataset
+
+class MITDataset(Dataset):
+    def __init__(self, data, targets):
+        self.data = data
+        self.targets = targets
+
+    def __getitem__(self, index):
+        return self.data[index], self.targets[index]
+
+    def __len__(self):
+        return len(self.data)
