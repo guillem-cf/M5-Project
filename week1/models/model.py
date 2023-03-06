916031c69c22beb4deef6ba906ecffbfb08b4f68
johnnynunez <johnnynuca14@gmail.com>
2023-03-07 00:46:38 +0100

    example model

---
+++ week1/models/model.py
@@ -0,0 +1,19 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+from week1.models.base_model import BaseModel
+from week1.models.components.simple_dense_net import SimpleDenseNet
+
+
+class Model(BaseModel):
+    def __init__(self, in_features, out_features):
+        super().__init__()
+        self.dense = SimpleDenseNet(in_features, out_features)
+
+    def forward(self, x):
+        x = self.dense(x)
+        return x
+
+if __name__ == '__main__':
+    model = Model(784, 10)
+    print(model)
