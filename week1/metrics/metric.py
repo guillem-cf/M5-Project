1e67ac332e0f54fddfd2c65b3635314ec3a0540c
johnnynunez <johnnynuca14@gmail.com>
2023-03-07 01:09:15 +0100

    WIP

--- week1/metrics/metric.py
+++ week1/metrics/metric.py
@@ -1,21 +1,20 @@
 import torch.nn.functional as F
+import torch
+
+def accuracy(output, target):
+    with torch.no_grad():
+        pred = torch.argmax(output, dim=1)
+        assert pred.shape[0] == len(target)
+        correct = 0
+        correct += torch.sum(pred == target).item()
+    return correct / len(target)
 
 
-def accuracy(output, target, topk=(1,)):
-    """Computes the precision@k for the specified values of k"""
-    maxk = max(topk)
-    batch_size = target.size(0)
-
-    _, pred = output.topk(maxk, 1, True, True)
-    pred = pred.t()
-    correct = pred.eq(target.view(1, -1).expand_as(pred))
-
-    res = []
-    for k in topk:
-        correct_k = correct[:k].view(-1).float().sum(0)
-        res.append(correct_k.mul_(100.0 / batch_size))
-    return res
-
-
-def cross_entropy(output, target):
-    return F.cross_entropy(output, target)
+def top_k_acc(output, target, k=3):
+    with torch.no_grad():
+        pred = torch.topk(output, k, dim=1)[1]
+        assert pred.shape[0] == len(target)
+        correct = 0
+        for i in range(k):
+            correct += torch.sum(pred[:, i] == target).item()
+    return correct / len(target)
