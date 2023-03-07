42104bbca9414cb87450d67e804833ef9be1253d
guillem-cf <1659365@uab.cat>
2023-03-07 19:49:51 +0100

    mit.py dataset

--- week1/datamodules/datasets/mit.py
+++
@@ -1,6 +0,0 @@
-from week1.datamodules.datasets.base_dataset import BaseDataset
-
-
-class MITDataset(BaseDataset):
-    def __init__(self, data, targets, transform=None, target_transform=None):
-        super().__init__(data, targets, transform, target_transform)
