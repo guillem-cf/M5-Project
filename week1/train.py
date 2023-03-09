cfecd8bb00d18e4c4d25b720bc1ac1787c581e78
guillem-cf <1659365@uab.cat>
2023-03-09 10:39:39 +0100

    .

--- week1/train.py
+++ week1/train.py
@@ -65,11 +65,11 @@ def train(args):
              transforms.ToTensor(),
              transforms.Resize((wandb.config.IMG_HEIGHT, wandb.config.IMG_WIDTH), antialias=False)])
 
-    train_dataset = MITDataset(data_dir='./MIT_small_train_1', split_name='train',
+    train_dataset = MITDataset(data_dir='/ghome/group03/mcv/m3/datasets/MIT_small_train_1', split_name='train',
                                transform=transform)
     train_loader = DataLoader(train_dataset, batch_size=wandb.config.BATCH_SIZE, shuffle=True, num_workers=8)
 
-    val_dataset = MITDataset(data_dir='./MIT_small_train_1', split_name='test',
+    val_dataset = MITDataset(data_dir='/ghome/group03/mcv/m3/datasets/MIT_small_train_1', split_name='test',
                              transform=transform)
     val_loader = DataLoader(val_dataset, batch_size=wandb.config.BATCH_SIZE, shuffle=False, num_workers=8)
 
