07dac3f7482e7bd088a3cddfdc9e7d4c5fd78ef4
Abel <64910220+abel-gr@users.noreply.github.com>
2023-03-22 21:15:49 +0100

    Update task_b.py

--- week3/task_b.py
+++ week3/task_b.py
@@ -84,10 +84,6 @@ if __name__ == '__main__':
     for d in random.sample(dataset_dicts, len(dataset_dicts)):
         im = cv2.imread(d["file_name"])
         outputs = predictor(im)
-        v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
-        out = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-
-        cv2.imwrite(output_path + d["file_name"].split('/')[-1], out.get_image()[:, :, ::-1])
 
         print("Processed image: " + d["file_name"].split('/')[-1])
 
@@ -100,6 +96,7 @@ if __name__ == '__main__':
 
 
 
+
     mask = np.nonzero(np.sum(co_ocurrence_matrix, axis=0) > 35)[0]
     co_ocurrence_matrix = co_ocurrence_matrix[np.ix_(mask, mask)]
 
@@ -109,3 +106,59 @@ if __name__ == '__main__':
     heatmap.set_yticklabels(heatmap.get_yticklabels(), rotation=0, fontsize=15)
     heatmap.set_xticklabels(heatmap.get_xticklabels(), rotation=45, fontsize=15)
     fig.savefig("task_b_heatmap.png")
+
+
+
+    for d in random.sample(dataset_dicts, len(dataset_dicts)):
+        im = cv2.imread(d["file_name"])
+        outputs = predictor(im)
+
+        if classes.index('person') not in outputs["instances"].to("cpu").pred_classes.tolist():
+            continue
+
+        # We look for a second image
+        for d2 in random.sample(dataset_dicts, len(dataset_dicts)):
+            im2 = cv2.imread(d2["file_name"])
+            outputs2 = predictor(im2)
+
+            classes2 = outputs2["instances"].to("cpu").pred_classes.tolist()
+
+            if classes.index('car') not in classes2 and classes.index('skis') not in classes2 and classes.index('dining table') not in classes2:
+                continue
+            else:
+
+                if classes.index('car') in classes2:
+                    class_index = classes.index('car')
+                
+                if classes.index('skis') in classes2:
+                    class_index = classes.index('skis')
+                    
+                if classes.index('dining table') in classes2:
+                    class_index = classes.index('dining table')
+
+                mask = outputs2["instances"].to("cpu").pred_masks[classes2.index(class_index)]
+                
+                a = np.where(mask != False)
+                im2b = im2[np.min(a[0]):np.max(a[0])+1, np.min(a[1]):np.max(a[1])+1]
+                maskb = mask[np.min(a[0]):np.max(a[0])+1, np.min(a[1]):np.max(a[1])+1]
+
+                im2c = np.where(np.repeat(np.expand_dims(maskb, axis=2), 3, axis=-1), im2b, -1)
+
+                if im2c.shape[0] >= im.shape[0] or im2c.shape[1] >= im.shape[1]:
+                    continue
+
+                im3 = np.copy(im)
+                p0, p1 = np.random.uniform(low=0, high=im.shape[0]-im2c.shape[0], size=(1)).astype(int)[0], np.random.uniform(low=0, high=im.shape[1]-im2c.shape[1], size=(1)).astype(int)[0]
+                im3[p0:p0+im2c.shape[0], p1:p1+im2c.shape[1]] = im2c
+
+                im3 = np.where(im3==-1, im, im3)
+
+                outputs3 = predictor(im3)
+                v = Visualizer(im3[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+                out = v.draw_instance_predictions(outputs3["instances"].to("cpu"))
+
+                cv2.imwrite(output_path + d["file_name"].split('/')[-1], out.get_image()[:, :, ::-1])
+
+                print("Processed image: " + d["file_name"].split('/')[-1])
+
+                break
