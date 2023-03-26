a80fa62edb1d1039617d3d9100829dba5725be0d
johnnynunez <johnnynuca14@gmail.com>
2023-03-26 12:36:43 +0200

    .

--- week3/task_e/utils.py
+++ week3/task_e/utils.py
@@ -20,19 +20,29 @@ def get_num(filename):
     else:
         return -1
 
+
 class Normalization(nn.Module):
     def __init__(self, mean, std):
-        super().__init__()
-        self.mean = nn.Parameter(torch.tensor(mean).to(device), requires_grad=False)
-        self.std = nn.Parameter(torch.tensor(std).to(device), requires_grad=False)
+        super(Normalization, self).__init__()
+        # .view the mean and std to make them [C x 1 x 1] so that they can
+        # directly work with image Tensor of shape [B x C x H x W].
+        # B is batch size. C is number of channels. H is height and W is width.
+        self.mean = torch.tensor(mean).view(-1, 1, 1)
+        self.std = torch.tensor(std).view(-1, 1, 1)
 
     def forward(self, img):
+        # normalize img
         return (img - self.mean) / self.std
 
 
 class ContentLoss(nn.Module):
-    def __init__(self, target):
-        super().__init__()
+
+    def __init__(self, target, ):
+        super(ContentLoss, self).__init__()
+        # we 'detach' the target content from the tree used
+        # to dynamically compute the gradient: this is a stated value,
+        # not a variable. Otherwise the forward method of the criterion
+        # will throw an error.
         self.target = target.detach()
 
     def forward(self, input):
@@ -52,27 +62,45 @@ class StyleLoss(nn.Module):
 
 
 def gram_matrix(input):
-    a, b, c, d = input.size()
-    features = input.view(a * b, c * d)
-    G = torch.mm(features, features.t())
+    a, b, c, d = input.size()  # a=batch size(=1)
+    # b=number of feature maps
+    # (c,d)=dimensions of a f. map (N=c*d)
+
+    features = input.view(a * b, c * d)  # resise F_XL into \hat F_XL
+
+    G = torch.mm(features, features.t())  # compute the gram product
+
+    # we 'normalize' the values of the gram matrix
+    # by dividing by the number of element in each feature maps.
     return G.div(a * b * c * d)
 
 
-def get_style_model_and_losses(cnn, normalization_mean, normalization_std, style_img, content_img,
-                               content_layers=['conv_4'],
-                               style_layers=['conv_1', 'conv_2', 'conv_3', 'conv_4', 'conv_5']):
-    cnn = copy.deepcopy(cnn)
+def get_style_model_and_losses(cnn, normalization_mean, normalization_std,
+                               style_img, content_img,
+                               content_layers,
+                               style_layers):
+    # normalization module
     normalization = Normalization(normalization_mean, normalization_std).to(device)
+
+    # just in order to have an iterable access to or list of content/syle
+    # losses
     content_losses = []
     style_losses = []
+
+    # assuming that cnn is a nn.Sequential, so we make a new nn.Sequential
+    # to put in modules that are supposed to be activated sequentially
     model = nn.Sequential(normalization)
-    i = 0
+
+    i = 0  # increment every time we see a conv
     for layer in cnn.children():
         if isinstance(layer, nn.Conv2d):
             i += 1
             name = 'conv_{}'.format(i)
         elif isinstance(layer, nn.ReLU):
             name = 'relu_{}'.format(i)
+            # The in-place version doesn't play very nicely with the ContentLoss
+            # and StyleLoss we insert below. So we replace with out-of-place
+            # ones here.
             layer = nn.ReLU(inplace=False)
         elif isinstance(layer, nn.MaxPool2d):
             name = 'pool_{}'.format(i)
@@ -80,21 +108,30 @@ def get_style_model_and_losses(cnn, normalization_mean, normalization_std, style
             name = 'bn_{}'.format(i)
         else:
             raise RuntimeError('Unrecognized layer: {}'.format(layer.__class__.__name__))
+
         model.add_module(name, layer)
+
         if name in content_layers:
+            # add content loss:
             target = model(content_img).detach()
             content_loss = ContentLoss(target)
             model.add_module("content_loss_{}".format(i), content_loss)
             content_losses.append(content_loss)
+
         if name in style_layers:
+            # add style loss:
             target_feature = model(style_img).detach()
             style_loss = StyleLoss(target_feature)
             model.add_module("style_loss_{}".format(i), style_loss)
             style_losses.append(style_loss)
+
+    # now we trim off the layers after the last content and style losses
     for i in range(len(model) - 1, -1, -1):
         if isinstance(model[i], ContentLoss) or isinstance(model[i], StyleLoss):
             break
+
     model = model[:(i + 1)]
+
     return model, style_losses, content_losses
 
 
