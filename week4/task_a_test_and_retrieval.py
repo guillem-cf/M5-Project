98aca02e14fbc6f767a28b24b26960a397ae80ff
Abel <64910220+abel-gr@users.noreply.github.com>
2023-03-29 21:15:45 +0200

    Add files via upload

--- week4/task_a_test_and_retrieval.py
+++ week4/task_a_test_and_retrieval.py
@@ -10,6 +10,7 @@ from sklearn.metrics import precision_score, average_precision_score, accuracy_s
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.manifold import TSNE
 
 finetuned = True
 dataset_path = '../../mcv/datasets/MIT_split'
@@ -133,8 +134,7 @@ with torch.no_grad():
 
         image_features_test = np.concatenate((image_features_test, outputs), axis=0)
 
-        if idx < 50:
-            test_images = np.concatenate((test_images, images.to("cpu").detach().numpy()), axis=0)
+        test_images = np.concatenate((test_images, images.to("cpu").detach().numpy()), axis=0)
 
 y_true_test = np.asarray(y_true_test).flatten()
 
@@ -197,17 +197,59 @@ print("mAP@5:", mAP_5)
 
 # Qualitative results:
 
-fig, ax = plt.subplots(6, 6, figsize=(10, 10), dpi=200)
-for i in range(0, 6):
-    ax[i][0].imshow(np.moveaxis(test_images[i], 0, -1))
-    ax[i][0].set_title("Test set\n query\n image")
-    ax[i][0].set_xticks([])
-    ax[i][0].set_yticks([])
-    for j in range(1, 6):
-        ax[i][j].imshow(np.moveaxis(train_images[neigh_ind[i][j]], 0, -1))
-        ax[i][j].set_title("Retrieved image\n at distance:\n" + str(round(neigh_dist[i][j], 4)))
-        ax[i][j].set_xticks([])
-        ax[i][j].set_yticks([])
-fig.tight_layout()
-plt.savefig("Results/Task_a/ImageRetrievalQualitativeResults.png")
-plt.close()
+def plot_retrieval(test_images, train_images, y_true_test, y_true_train, neigh_ind, neigh_dist, p="BEST", num_queries=6, num_retrievals=6):
+
+    if p=="BEST":
+        ind = np.argsort(np.sum(neigh_dist[:, 0:5], axis=1), axis=0)
+        title = "Test query images that obtained retrieved images \nwith the lowest distance among the dataset\n"
+    elif p=="WORST":
+        ind = np.argsort(-np.sum(neigh_dist[:, 0:5], axis=1), axis=0)
+        title = "Test query images that obtained retrieved images \nwith the highest distance among the dataset\n"
+    else:
+        title = "First test query images and their retrieved train images\n"
+
+    if p=="BEST" or p=="WORST":
+        test_images = test_images[ind]
+        y_true_test = y_true_test[ind]
+        neigh_ind = neigh_ind[ind]
+        neigh_dist = neigh_dist[ind]
+
+    fig, ax = plt.subplots(num_queries, num_retrievals, figsize=(10, 10), dpi=200)
+    fig.suptitle(title)
+    for i in range(0, num_queries):
+        ax[i][0].imshow(np.moveaxis(test_images[i], 0, -1))
+        ax[i][0].set_title("Test set \nquery image \nClass: " + str(y_true_test[i]))
+        ax[i][0].set_xticks([])
+        ax[i][0].set_yticks([])
+        for j in range(1, num_retrievals):
+            ax[i][j].imshow(np.moveaxis(train_images[neigh_ind[i][j]], 0, -1))
+            ax[i][j].set_title("Retrieved image\n Distance: " + str(round(neigh_dist[i][j], 2)) + "\nClass: " + str(y_true_train[neigh_ind[i][j]]))
+            ax[i][j].set_xticks([])
+            ax[i][j].set_yticks([])
+    fig.tight_layout()
+    plt.savefig("Results/Task_a/ImageRetrievalQualitativeResults_" + p + ".png")
+    plt.close()
+
+plot_retrieval(test_images, train_images, y_true_test, y_true_train, neigh_ind, neigh_dist, p="")
+plot_retrieval(test_images, train_images, y_true_test, y_true_train, neigh_ind, neigh_dist, p="BEST")
+plot_retrieval(test_images, train_images, y_true_test, y_true_train, neigh_ind, neigh_dist, p="WORST")
+
+# TSNE of features
+
+def tsne_features(image_features, y_true, subset):
+
+    tsne = TSNE(n_components=2)
+    X_tsne = tsne.fit_transform(image_features, y_true)
+
+    fig, ax = plt.subplots(figsize=(10, 10), dpi=200)
+    colors = ['red', 'orange', 'yellow', 'green', 'cyan', 'blue', 'purple', 'pink']
+    for i, c in enumerate(set(y_true)):
+        mask = (y_true == c)
+        ax.scatter(X_tsne[mask, 0], X_tsne[mask, 1], label=test_dataset.classes[i], c=colors[i], alpha=0.7)
+    ax.legend()
+    ax.set_title("t-SNE of the " + subset + " images features\n (ResNet50 output of last convolutional layer)")
+    fig.savefig("Results/Task_a/tsne_" + subset + "_features.png")
+
+
+tsne_features(image_features_train, y_true_train, "train")
+tsne_features(image_features_test, y_true_test, "test")
