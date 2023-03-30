e5f4a7a0c15f107593c09d3e89fabe9937df4a07
johnnynunez <johnnynuca14@gmail.com>
2023-03-31 01:36:31 +0200

    WORKING

--- week4/dataset/siamese_data.py
+++ week4/dataset/siamese_data.py
@@ -3,38 +3,31 @@ import os
 import torch
 from PIL import Image
 from torch.utils.data import Dataset
+from torchvision.datasets import ImageFolder
 
 
 class SiameseMITDataset(Dataset):
     def __init__(self, data_dir, split_name='train', transform=None):
         self.data_dir = os.path.join(data_dir, split_name)
         self.transform = transform
-        self.classes = sorted(os.listdir(self.data_dir))
-        self.class_to_idx = {self.classes[i]: i for i in range(len(self.classes))}
+        self.dataset = ImageFolder(root=self.data_dir, transform=None)
         self.samples = []
-        for target in self.classes:
-            d = os.path.join(self.data_dir, target)
-            if not os.path.isdir(d):
-                continue
-            for root, _, fnames in sorted(os.walk(d)):
-                for fname1 in sorted(fnames):
-                    path1 = os.path.join(root, fname1)
-                    item1 = (path1, self.class_to_idx[target])
-                    for fname2 in sorted(fnames):
-                        path2 = os.path.join(root, fname2)
-                        item2 = (path2, self.class_to_idx[target])
-                        self.samples.append((item1, item2))
+        for i in range(len(self.dataset)):
+            for j in range(i+1, len(self.dataset)):
+                item1 = self.dataset[i]
+                item2 = self.dataset[j]
+                self.samples.append((item1, item2))
 
     def __len__(self):
         return len(self.samples)
 
     def __getitem__(self, idx):
         item1, item2 = self.samples[idx]
-        path1, target1 = item1
-        path2, target2 = item2
+        path1, target1 = item1[0], item1[1]
+        path2, target2 = item2[0], item2[1]
 
-        sample1 = Image.open(path1).convert('RGB')
-        sample2 = Image.open(path2).convert('RGB')
+        sample1 = item1[0]
+        sample2 = item2[0]
 
         if self.transform:
             sample1 = self.transform(sample1)
--- week4/dataset/triplet_data.py
+++ week4/dataset/triplet_data.py
@@ -3,38 +3,32 @@ import random
 
 from PIL import Image
 from torch.utils.data import Dataset
+from torchvision.datasets import ImageFolder
 
 
 class TripletMITDataset(Dataset):
     def __init__(self, data_dir='/ghome/group03/mcv/m3/datasets/MIT_small_train_1', split_name='train', transform=None):
         self.data_dir = os.path.join(data_dir, split_name)
         self.transform = transform
-        self.classes = sorted(os.listdir(self.data_dir))
-        self.class_to_idx = {self.classes[i]: i for i in range(len(self.classes))}
+        self.dataset = ImageFolder(root=self.data_dir, transform=None)
         self.samples = []
-        for target in self.classes:
-            d = os.path.join(self.data_dir, target)
-            if not os.path.isdir(d):
-                continue
-            for root, _, fnames in sorted(os.walk(d)):
-                for i, fname in enumerate(sorted(fnames)):
-                    path = os.path.join(root, fname)
-                    item = (path, self.class_to_idx[target], i)
-                    self.samples.append(item)
+        for i in range(len(self.dataset)):
+            path, target = self.dataset.imgs[i]
+            self.samples.append((path, target))
 
     def __len__(self):
         return len(self.samples)
 
     def __getitem__(self, index):
-        anchor_path, anchor_target, anchor_idx = self.samples[index]
+        anchor_path, anchor_target = self.samples[index]
 
         # choose a positive example from the same class as the anchor
-        positive_candidates = [s for s in self.samples if s[1] == anchor_target and s[2] != anchor_idx]
-        positive_path, _, _ = random.choice(positive_candidates)
+        positive_candidates = [s for s in self.samples if s[1] == anchor_target and s[0] != anchor_path]
+        positive_path, _ = random.choice(positive_candidates)
 
         # choose a negative example from a different class than the anchor
         negative_candidates = [s for s in self.samples if s[1] != anchor_target]
-        negative_path, _, _ = random.choice(negative_candidates)
+        negative_path, _ = random.choice(negative_candidates)
 
         anchor_sample = Image.open(anchor_path).convert('RGB')
         positive_sample = Image.open(positive_path).convert('RGB')
