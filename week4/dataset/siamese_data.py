79cd5cf52285ca0ec012d0a79e858e5f7322cad0
johnnynunez <johnnynuca14@gmail.com>
2023-03-31 02:10:31 +0200

    WORKING

--- week4/dataset/siamese_data.py
+++ week4/dataset/siamese_data.py
@@ -1,36 +1,73 @@
 import os
 
 import torch
-from PIL import Image
 from torch.utils.data import Dataset
 from torchvision.datasets import ImageFolder
+from torchvision.transforms import ToTensor, Normalize
+import numpy as np
+from PIL import Image
 
 
 class SiameseMITDataset(Dataset):
-    def __init__(self, data_dir, split_name='train', transform=None):
+    def __init__(self, data_dir='/ghome/group03/mcv/m3/datasets/MIT_small_train_1', split_name='train', transform=None):
         self.data_dir = os.path.join(data_dir, split_name)
+        self.dataset = ImageFolder(data_dir, transform=transform)
+        self.n_samples = len(self.dataset)
+        self.split_name = split_name
         self.transform = transform
-        self.dataset = ImageFolder(root=self.data_dir, transform=None)
-        self.samples = []
-        for i in range(len(self.dataset)):
-            for j in range(i+1, len(self.dataset)):
-                item1 = self.dataset[i]
-                item2 = self.dataset[j]
-                self.samples.append((item1, item2))
+
+        if self.split_name == 'train':
+            self.labels = self.dataset.targets
+            self.data = self.dataset.samples
+            self.label_to_indices = {label: np.where(np.asarray(self.labels) == label)[0] for label in
+                                     np.unique(self.labels)}
+        else:
+            self.test_labels = self.dataset.targets
+            self.test_data = self.dataset.samples
+            self.labels_set = set(self.test_labels)
+            self.label_to_indices = {label: np.where(np.asarray(self.test_labels) == label)[0]
+                                     for label in self.labels_set}
+
+            random_state = np.random.RandomState(2023)
+
+            # Generate fixed pairs for testing
+            positive_pairs = [[i, random_state.choice(self.label_to_indices[self.test_labels[i]]), 1]
+                              for i in range(0, len(self.test_data), 2)]
+
+            negative_pairs = [[i, random_state.choice(
+                self.label_to_indices[np.random.choice(list(self.labels_set - {self.test_labels[i]}))]), 0]
+                              for i in range(1, len(self.test_data), 2)]
+
+            self.test_pairs = positive_pairs + negative_pairs
+
+    def __getitem__(self, index):
+        if self.split_name == 'train':
+            target = np.random.randint(0, 2)  # 0 or 1 (negative or positive)
+            img1, label1 = self.data[index], self.labels[index]
+
+            if target == 1:
+                siamese_index = index
+                while siamese_index == index:
+                    siamese_index = np.random.choice(self.label_to_indices[label1])
+            else:
+                siamese_label = np.random.choice(list(self.label_to_indices.keys() - {label1}))
+                siamese_index = np.random.choice(self.label_to_indices[siamese_label])
+
+            img2 = self.data[siamese_index]
+
+        else:
+            img1 = self.test_data[self.test_pairs[index][0]]
+            img2 = self.test_data[self.test_pairs[index][1]]
+            target = self.test_pairs[index][2]
+
+        img1 = Image.open(img1[0])
+        img2 = Image.open(img2[0])
+
+        if self.transform is not None:
+            img1 = self.transform(img1)
+            img2 = self.transform(img2)
+
+        return (img1, img2), torch.tensor(target)
 
     def __len__(self):
-        return len(self.samples)
-
-    def __getitem__(self, idx):
-        item1, item2 = self.samples[idx]
-        path1, target1 = item1[0], item1[1]
-        path2, target2 = item2[0], item2[1]
-
-        sample1 = item1[0]
-        sample2 = item2[0]
-
-        if self.transform:
-            sample1 = self.transform(sample1)
-            sample2 = self.transform(sample2)
-
-        return sample1, sample2, torch.tensor(target1 == target2, dtype=torch.float32)
+        return self.n_samples
--- week4/dataset/triplet_data.py
+++ week4/dataset/triplet_data.py
@@ -1,42 +1,73 @@
 import os
-import random
 
+import numpy as np
 from PIL import Image
+
 from torch.utils.data import Dataset
 from torchvision.datasets import ImageFolder
 
 
 class TripletMITDataset(Dataset):
+
     def __init__(self, data_dir='/ghome/group03/mcv/m3/datasets/MIT_small_train_1', split_name='train', transform=None):
         self.data_dir = os.path.join(data_dir, split_name)
+        self.dataset = ImageFolder(data_dir, transform=transform)
+        self.n_samples = len(self.dataset)
+        self.split_name = split_name
         self.transform = transform
-        self.dataset = ImageFolder(root=self.data_dir, transform=None)
-        self.samples = []
-        for i in range(len(self.dataset)):
-            path, target = self.dataset.imgs[i]
-            self.samples.append((path, target))
 
-    def __len__(self):
-        return len(self.samples)
+        if self.split_name == 'train':
+            self.train_labels = self.dataset.targets
+            self.train_data = self.dataset.samples
+            self.labels_set = set(self.train_labels)
+            self.label_to_indices = {label: np.where(np.asarray(self.train_labels) == label)[0]
+                                     for label in self.labels_set}
+
+        else:
+            self.test_labels = self.dataset.targets
+            self.test_data = self.dataset.samples
+            # generate fixed triplets for testing
+            self.labels_set = set(self.test_labels)
+            self.label_to_indices = {label: np.where(np.asarray(self.test_labels) == label)[0]
+                                     for label in self.labels_set}
+
+            random_state = np.random.RandomState(29)
+
+            triplets = [[i,
+                         random_state.choice(self.label_to_indices[self.test_labels[i]]),
+                         random_state.choice(self.label_to_indices[
+                                                 np.random.choice(
+                                                     list(self.labels_set - {self.test_labels[i]})
+                                                 )
+                                             ])
+                         ]
+                        for i in range(len(self.test_data))]
+            self.test_triplets = triplets
 
     def __getitem__(self, index):
-        anchor_path, anchor_target = self.samples[index]
+        if self.split_name == 'train':
+            img1, label1 = self.train_data[index], self.train_labels[index]
+            positive_index = index
+            while positive_index == index:
+                positive_index = np.random.choice(self.label_to_indices[label1])
+            negative_label = np.random.choice(list(self.labels_set - {label1}))
+            negative_index = np.random.choice(self.label_to_indices[negative_label])
+            img2 = self.train_data[positive_index]
+            img3 = self.train_data[negative_index]
+        else:
+            img1 = self.test_data[self.test_triplets[index][0]]
+            img2 = self.test_data[self.test_triplets[index][1]]
+            img3 = self.test_data[self.test_triplets[index][2]]
 
-        # choose a positive example from the same class as the anchor
-        positive_candidates = [s for s in self.samples if s[1] == anchor_target and s[0] != anchor_path]
-        positive_path, _ = random.choice(positive_candidates)
-
-        # choose a negative example from a different class than the anchor
-        negative_candidates = [s for s in self.samples if s[1] != anchor_target]
-        negative_path, _ = random.choice(negative_candidates)
-
-        anchor_sample = Image.open(anchor_path).convert('RGB')
-        positive_sample = Image.open(positive_path).convert('RGB')
-        negative_sample = Image.open(negative_path).convert('RGB')
+        img1 = Image.open(img1[0])
+        img2 = Image.open(img2[0])
+        img3 = Image.open(img3[0])
 
         if self.transform is not None:
-            anchor_sample = self.transform(anchor_sample)
-            positive_sample = self.transform(positive_sample)
-            negative_sample = self.transform(negative_sample)
+            img1 = self.transform(img1)
+            img2 = self.transform(img2)
+            img3 = self.transform(img3)
+        return (img1, img2, img3), []
 
-        return anchor_sample, positive_sample, negative_sample
+    def __len__(self):
+        return self.n_samples
