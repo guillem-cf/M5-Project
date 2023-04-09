49d414f51dc0a3fa4f3a06e5eae4777c46ebbf3a
guillem-cf <1659365@uab.cat>
2023-04-09 10:38:11 +0200

    .

--- week4/models/models.py
+++ week4/models/models.py
@@ -1,20 +1,15 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from torch.nn import Sequential
 
 from torchvision import models
-from torchvision.models import resnet18, resnet50, ResNet50_Weights
-from torchvision.models.detection import FasterRCNN_ResNet50_FPN_Weights, FasterRCNN
-from torchvision.models.detection.backbone_utils import BackboneWithFPN
-from torchvision.models.detection.roi_heads import RoIHeads
-from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models import resnet18, resnet50
+from torchvision.models.detection import FasterRCNN_ResNet50_FPN_Weights
 
 
 class EmbeddingNet(nn.Module):
     def __init__(self, weights, resnet_type='resnet50'):
         super(EmbeddingNet, self).__init__()
-        
+
         if resnet_type == 'resnet50':
             self.resnet = resnet50(weights=weights)
             self.resnet = nn.Sequential(*(list(self.resnet.children())[:-1]))
@@ -28,65 +23,65 @@ class EmbeddingNet(nn.Module):
         elif resnet_type == 'resnet18':
             self.resnet = resnet18(weights=weights)
             self.resnet = nn.Sequential(*(list(self.resnet.children())[:-1]))
-            
+
             self.fc = nn.Sequential(nn.Linear(512, 256),
                                     nn.PReLU(),
                                     nn.Linear(256, 256),
                                     nn.PReLU(),
                                     nn.Linear(256, 2)
                                     )
-        
+
     def forward(self, x):
         output = self.resnet(x).squeeze()
         # output = output.view(output.size()[0], -1)
         output = self.fc(output)
         return output
-    
+
     def get_embedding(self, x):
         return self.forward(x)
-    
-    
+
+
 # No updates in faster RCNN
 # https://pytorch.org/vision/main/models/generated/torchvision.models.detection.fasterrcnn_resnet50_fpn.html
 class ObjectEmbeddingNet(nn.Module):
     def __init__(self, weights, num_classes):
-        super(ObjectEmbeddingNet,self).__init__()
-        
+        super(ObjectEmbeddingNet, self).__init__()
+
         self.faster_rcnn = models.detection.fasterrcnn_resnet50_fpn(weights=FasterRCNN_ResNet50_FPN_Weights.DEFAULT)
-        
+
         # in_features = self.faster_rcnn.roi_heads.box_predictor.cls_score.in_features
         # self.faster_rcnn.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
-        
+
+        # Remove the softmax layer
+        self.faster_rcnn.roi_heads.box_predictor.cls_score = nn.Identity()
+
         self.fc = nn.Sequential(nn.Sequential(nn.Linear(2048, 256),
-                                nn.PReLU(),
-                                nn.Linear(256, 256),
-                                nn.PReLU(),
-                                nn.Linear(256, 2)
-                                ))
-        
-        
+                                              nn.PReLU(),
+                                              nn.Linear(256, 256),
+                                              nn.PReLU(),
+                                              nn.Linear(256, 2)
+                                              ))
+
     def forward(self, x, target):
-          
         # detections = self.faster_rcnn(x, target)
         # object_embeddings = []
-        
+
         images = list(x)
-        features = self.faster_rcnn.backbone(x) # Extract features from backbone
-        proposals, _ = self.faster_rcnn.rpn(images, features)                               # ARREGLAR AIXO
-        detections, _ = self.faster_rcnn.roi_heads(features, proposals, images.image_sizes) # ARREGLAR AIXO
- 
+        features = self.faster_rcnn.backbone(x)  # Extract features from backbone
+        proposals, _ = self.faster_rcnn.rpn(images, features)  # ARREGLAR AIXO
+        detections, _ = self.faster_rcnn.roi_heads(features, proposals, images.image_sizes)  # ARREGLAR AIXO
+
         object_embeddings = []
         for detection in detections:
             object_features = detection['features']
             object_embeddings.append(self.fc(object_features))
-        
+
         object_embeddings = torch.stack(object_embeddings).mean(dim=0)
-        
+
         return object_embeddings
-    
+
     def get_embedding(self, x):
         return self.forward(x)
-
 
 
 class SiameseNet(nn.Module):
@@ -103,7 +98,6 @@ class SiameseNet(nn.Module):
         return self.embedding_net(x)
 
 
-
 class TripletNet(nn.Module):
     def __init__(self, embedding_net):
         super(TripletNet, self).__init__()
@@ -117,9 +111,8 @@ class TripletNet(nn.Module):
 
     def get_embedding(self, x):
         return self.embedding_net(x)
-    
-    
-    
+
+
 class TripletNet_fasterRCNN(nn.Module):
     def __init__(self, embedding_net):
         super(TripletNet_fasterRCNN, self).__init__()
@@ -135,33 +128,29 @@ class TripletNet_fasterRCNN(nn.Module):
         return self.embedding_net(x)
 
 
-
-
 class BasicEmbeddingNet(nn.Module):
     def __init__(self):
         super(EmbeddingNet, self).__init__()
-                                           
+
         self.convnet = nn.Sequential(nn.Conv2d(1, 32, 5), nn.PReLU(),
-                        nn.MaxPool2d(2, stride=2),
-                        nn.Conv2d(32, 64, 5), nn.PReLU(),
-                        nn.MaxPool2d(2, stride=2))
-        
+                                     nn.MaxPool2d(2, stride=2),
+                                     nn.Conv2d(32, 64, 5), nn.PReLU(),
+                                     nn.MaxPool2d(2, stride=2))
+
         self.fc = nn.Sequential(nn.Linear(64 * 4 * 4, 256),
-                        nn.PReLU(),
-                        nn.Linear(256, 256),
-                        nn.PReLU(),
-                        nn.Linear(256, 2))
-        
+                                nn.PReLU(),
+                                nn.Linear(256, 256),
+                                nn.PReLU(),
+                                nn.Linear(256, 2))
+
     def forward(self, x):
         output = self.convnet(x)
         output = self.fc(output)
         return output
-    
+
     def get_embedding(self, x):
         return self.forward(x)
-    
-    
-    
+
 
 class ObjectEmbeddingNet_v2(nn.Module):
     def __init__(self, num_classes, feature_dim):
